64c616a9044e567358683e8d776974358175ce4e
IkHebEenHaarZiekte <97103329@st.deltion.nl>
2025-04-22 20:12:28 +0200

    opdr3

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -4,3 +4,11 @@
 
 # Hier komt je code...
 
+mylist = []
+
+for i in range(5):
+    object = input(f"Vul object {i+1} in: ")
+    mylist.append(object)
+
+    mylist_sorted = sorted(mylist, reverse=True)
+    print(mylist_sorted)
