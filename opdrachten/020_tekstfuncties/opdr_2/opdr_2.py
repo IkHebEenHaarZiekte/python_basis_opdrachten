80d4755b7b7dcb74bf2aec7faa84ff5d4f632a69
IkHebEenHaarZiekte <97103329@st.deltion.nl>
2025-04-22 17:03:43 +0200

    Opdr2

--- opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
@@ -4,6 +4,6 @@
 
 # Hier komt je code...
 
-my_list = ...
-
-print()
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"
+aantal_t = my_string.lower() .count("t")
+print(f'De letter "t" komt {aantal_t}x voor in my_string')
