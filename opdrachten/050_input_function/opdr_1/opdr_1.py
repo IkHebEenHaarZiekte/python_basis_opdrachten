57317ee266461eb7a279b0a30466abffc6c31b78
IkHebEenHaarZiekte <97103329@st.deltion.nl>
2025-04-22 19:41:42 +0200

    opdr1

--- opdrachten/050_input_function/opdr_1/opdr_1.py
+++ opdrachten/050_input_function/opdr_1/opdr_1.py
@@ -4,4 +4,11 @@
 
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
 
+import math
 
+a = float(input("Geef de lengte van de eerste zijde\n"))
+b = float(input("Geef de lengte van de tweede zijde\n"))
+
+c = math.sqrt(a**2 + b**2)
+
+print(f"\nDe lengte van de schuine zijde is: {c}")
