d06a0562800021e8876667c946c9005139cfcf77
IkHebEenHaarZiekte <97103329@st.deltion.nl>
2025-04-22 20:18:55 +0200

    opdr2

--- opdrachten/060_loops/opdr_2/opdr_2.py
+++ opdrachten/060_loops/opdr_2/opdr_2.py
@@ -1,10 +1,16 @@
 # Opdracht 2 loops
 # Naam student:
 # Groep:
+from threading import main_thread
 
 # Hier komt je code...
 
 # Hier start de for-loop
 
 my_list = []
+
+for i in range(10, 51, 10):
+    my_list.append(i)
+
+    print(my_list)
 
