7ca9f9512f3abcddb2797b605c62125f85a50b3b
IkHebEenHaarZiekte <97103329@st.deltion.nl>
2025-04-22 20:31:33 +0200

    opdr6

--- opdrachten/060_loops/opdr_6/opdr_6.py
+++ opdrachten/060_loops/opdr_6/opdr_6.py
@@ -6,4 +6,20 @@
 
 # Hier start de for-loop
 
-my_list = []
+pizza_list = ["margharita", "calzone", "verdi", "olivio", "quattro stagioni"]
+
+pizza_list.sort()
+print(pizza_list)
+
+pizza_list.append("yo-favorito")
+print(pizza_list)
+
+pizza_list.remove("olivio")
+print(pizza_list)
+
+print(pizza_list[:3])
+
+middle_index = len(pizza_list) // 2
+print([pizza_list[middle_index]])
+
+print(pizza_list[-3:])
