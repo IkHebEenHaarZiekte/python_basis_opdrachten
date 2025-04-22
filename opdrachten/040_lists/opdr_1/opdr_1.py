2bbab4674f0ef1dad97b0f3d1f6fc1014762ce29
IkHebEenHaarZiekte <97103329@st.deltion.nl>
2025-04-22 18:42:37 +0200

    opdr1

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -2,8 +2,15 @@
 # Naam student:
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+mylist = []
+dict_1 = {"id": 0, "voornaam": "Lars", "achternaam": "de Jong"}
+dict_2 = {"id": 1, "voornaam": "Emma", "achternaam": "Bakker"}
+dict_3 = {"id": 2, "voornaam": "Noah", "achternaam": "Visser"}
+dict_4 = {"id": 3, "voornaam": "Sophie", "achternaam": "Smits"}
+
+mylist.append(dict_1)
+mylist.append(dict_2)
+mylist.append(dict_3)
+mylist.append(dict_4)
+
+print(mylist[1]["voornaam"], mylist[1]["achternaam"])
