8e7b71cf665c33b938c1283d7f218aaca2b6fe89
IkHebEenHaarZiekte <97103329@st.deltion.nl>
2025-04-22 18:23:12 +0200

    opdr2

--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -4,7 +4,20 @@
 
 # Hier komt je code...
 
-c = ...
-f = ...
+c = -12
+f = 102
 
-print()
+c_to_f = (c * 9/5) + 32
+f_to_c = (f - 32) * 5/9
+
+print(f"{c} graden Celsius is gelijk aan {round(c_to_f, 1)} graden Fahrenheit")
+print(f"{f} graden Fahrenheit is gelijk aan {round(f_to_c, 1)} graden Celsius")
+
+c = 62.2
+f = 96
+
+c_to_f = (c * 9/5) + 32
+f_to_c = (f - 32) * 5/9
+
+print(f"{c} graden Celius is gelijk aan {round(c_to_f, 1)} graden Fahrenheit")
+print(f"{f} graden Fahrenheit is gelijk aan {round(f_to_c, 1)} graden Celsius")
