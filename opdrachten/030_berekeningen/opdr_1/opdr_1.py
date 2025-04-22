6127712171e66d6f6edf1b813eeb14251f34c773
IkHebEenHaarZiekte <97103329@st.deltion.nl>
2025-04-22 18:28:14 +0200

    opdr1

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,14 @@
 # Groep:
 
 # Hier komt je code...
+
+joe = 23
+bob = 15
+fred = 22
+
+totaal = joe + bob + fred
+gemiddelde = totaal / 3
+
+print("Dit weten we over joe, bob en fred:")
+print(f"De totale waarde van alle leeftijden bij elkaar opgeteld = {totaal}")
+print(f"De gemiddelde leeftijf van deze boys is: {round(gemiddelde)}")
