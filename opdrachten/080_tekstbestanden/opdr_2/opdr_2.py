ec6635643fad6147db7015b98272cdc779e84249
IkHebEenHaarZiekte <97103329@st.deltion.nl>
2025-04-24 20:41:22 +0200

    opdr2

--- opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
+++ opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
@@ -3,8 +3,26 @@
 # Groep:
 
 import random
+from operator import truediv
+
 prompt = "Raad mijn geheime getal \n"
 geheim_getal = random.randint(1, 100)
+
+pogingen = 0
+geraden = False
+
+while not geraden:
+    gok = int(input(prompt))
+    pogingen += 1
+
+    if gok < geheim_getal:
+        print("Hoger")
+    elif gok > geheim_getal:
+        print("Lager")
+    else:
+        print(f"Je hebt het getal geraden het is {geheim_getal}")
+        print(f"Je hebt het in {pogingen} keer gedaan")
+        geraden = True
 
 # De rest moet jij doen.....
 
