523fa19983bfde5b0ca3c4964b5137b78eaada46
IkHebEenHaarZiekte <97103329@st.deltion.nl>
2025-04-24 20:04:48 +0200

    opdr3

--- opdrachten/070_condities/opdr_3/opdr_3.py
+++ opdrachten/070_condities/opdr_3/opdr_3.py
@@ -1,12 +1,27 @@
 # Opdracht 3 condities
 # Naam student:
 # Groep:
-
-
-
+from enum import nonmember
 
 normale_toegangsprijs = 12.50
 kortings_percentages = {"baby": 100, "kinderen": 50, "volwassenen": 0, "ouderen": 30}
 leeftijd = {"baby": (0, 2), "kinderen": (3, 18), "volwassenen": (19, 64), "ouderen": (65, 150)}
 
 # Hier komt je code...
+
+leeftijd_input = int(input("Geef uw leeftijd in aantal jaar\n"))
+
+groep = None
+for naam, (min_leeftijd, max_leeftijd) in leeftijd.items():
+    if min_leeftijd <= leeftijd_input <= max_leeftijd:
+        groep = naam
+        break
+if groep:
+    korting = kortings_percentages[groep]
+    te_betalen = normale_toegangsprijs * (1 - korting / 100)
+
+    print(f"U behoort tot de groep {groep}")
+    print(f"U krijgt {korting}% korting")
+    print(f"U betaalt daarom {round(te_betalen, 2)}")
+else:
+    print("Leeftijd valt buiten de bekende categorieeën")
