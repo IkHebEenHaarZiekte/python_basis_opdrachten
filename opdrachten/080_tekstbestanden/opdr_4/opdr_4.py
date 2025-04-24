cf1116cef2d66c02aabebc64c965d29584062ab9
IkHebEenHaarZiekte <97103329@st.deltion.nl>
2025-04-24 21:26:18 +0200

    opdr4

--- opdrachten/080_tekstbestanden/opdr_4/opdr_4.py
+++ opdrachten/080_tekstbestanden/opdr_4/opdr_4.py
@@ -4,3 +4,26 @@
 
 
 # Party enquete
+
+vragen = [
+    "Wat is je voornaam?",
+    "Wat is je achternaam?",
+    "Wat neem je mee aan drank?",
+    "Wat neem je mee om te eten?"
+]
+
+antwoorden = {}
+labels = ["voornaam", "achternaam", "drank", "eten"]
+
+for i, vraag in enumerate(vragen):
+    antwoord = input(f"{i+1}. {vraag} \n")
+    antwoorden[labels[i]] = antwoord
+
+    print("\nBedankt voor het invullen!")
+    print("See you at the party!")
+
+    with open("feestgangers.txt", "a", encoding="utf-8") as bestand:
+        bestand.write("----\n")
+        for sleutel, waarde in antwoorden.items():
+            bestand.write(f"{sleutel}: {waarde}\n")
+            bestand.write("\n")
