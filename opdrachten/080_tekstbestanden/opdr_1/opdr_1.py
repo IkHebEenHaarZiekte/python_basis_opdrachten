1562813307b104f7510521668e110e1fd1ef19cb
IkHebEenHaarZiekte <97103329@st.deltion.nl>
2025-04-24 20:35:27 +0200

    opdr1

--- opdrachten/080_tekstbestanden/opdr_1/opdr_1.py
+++ opdrachten/080_tekstbestanden/opdr_1/opdr_1.py
@@ -3,3 +3,21 @@
 # Groep:
 
 # Jouw code komt hier
+
+vragen = [
+    "Wat vind je van de houdige regering?",
+    "Wat vind je van de Python-lessen tot nu toe?",
+    "Wat vind jij de mooiste stad van Nederland?"
+]
+
+antwoorden = []
+
+for vraag in vragen:
+    antwoord = input(vraag + "/n")
+    antwoorden.append(antwoord)
+
+with open("enquete_resultaten.txt", "w", encoding="utf-8") as bestand:
+    for i in range(len(vragen)):
+        bestand.write(f"{vragen[i]}/nAntwoord: {antwoorden[i]}/n/n")
+
+        print("Bedankt voor het invullen! De resultaten zijn opgeslagen in enquete_resultaten.txt'.")
