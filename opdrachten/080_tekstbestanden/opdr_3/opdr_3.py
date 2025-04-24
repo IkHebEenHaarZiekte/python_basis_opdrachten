ac4f4ae0cb0dea229d7ae30bdcac97102674c2bc
IkHebEenHaarZiekte <97103329@st.deltion.nl>
2025-04-24 21:14:46 +0200

    opdr3

--- opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
+++ opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
@@ -2,5 +2,22 @@
 # Naam student:
 # Groep:
 
+def encrypt_tekst(tekst, verschuiving):
+    resultaat = ""
 
+    for teken in tekst:
+        if teken.isalpha():
+            basis = ord('A') if teken.isupper() else ord('a')
+            verschoven = (ord(teken) - basis + verschuiving) % 26 + basis
+            resultaat += chr(verschoven)
+        else:
+            resultaat += teken
 
+    return resultaat
+
+invoer = input("Geef de tekst die je wilt encrypten..\n")
+
+gecodeert = encrypt_tekst(invoer, 5)
+
+print("\nGeëncrypte tekst:")
+print(gecodeert)
